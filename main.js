b1d07fedaff41cddf0e6a11d1187a8e779cf7ecb
eliseucostaalbano <ELISEUALBANO57@GMAIL.COM>
2025-09-22 13:54:23 -0300

    colocandoas imagens e um refletor

--- main.js
+++ main.js
@@ -1,12 +1,14 @@
 import * as THREE from 'three';
+import { Reflector } from 'three/addons/objects/Reflector.js';
+
 
 const imagens = [
-  'socrates.jpg',
-  'stars.jpg',
-  'wave.jpg',
-  'spring.jpg',
-  'mountain.jpg',
-  'sunday.jpg'
+  './img/socrates.jpg',
+  './img/stars.jpg',
+  './img/wave.jpg',
+  './img/spring.jpg',
+  './img/mountain.jpg',
+  './img/sunday.jpg'
 ];
 
 const titulos = [
@@ -27,6 +29,7 @@ const artistas = [
   'George Seurat'
 ];
 
+const textureLoader = new THREE.TextureLoader();
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 renderer.setAnimationLoop( animate );
@@ -40,18 +43,46 @@ cena.add(root);
 
 let count = 6;
 for (let i = 0; i < count; i++) {
+  const textura = textureLoader.load( imagens[i] );
+  textura.colorSpace = THREE.SRGBColorSpace;
   const base = new THREE.Object3D();
   base.rotation.y = i *  (2 * Math.PI / count);
   root.add(base);
 
+const borda = new THREE.Mesh(
+    new THREE.BoxGeometry(3.2, 2.2, 0.09),
+    new THREE.MeshStandardMaterial({ color: 0x202020 })
+);
+borda.position.z = -4.05;
+base.add( borda );
+
 const arte = new THREE.Mesh(
   new THREE.BoxGeometry(3, 2, 0.01),
-  new THREE.MeshBasicMaterial({ color: 0xf08080 })
+  new THREE.MeshStandardMaterial({ map: textura })
 );
 arte.position.z = -4;
 base.add( arte );
 
 }
+
+const holoFote = new THREE.SpotLight(0xffffff, 100.0, 10, 0.65, 1);
+holoFote.position.set(0, 5, 0);
+holoFote.target.position.set(0, 1, -5);
+cena.add(holoFote);
+cena.add(holoFote.target);
+
+const espelho = new Reflector(
+  new THREE.CircleGeometry(10),
+  {
+    color: 0x505050,
+   textureWidth: window.innerWidth,
+   textureHeight: window.innerHeight,
+  }
+);
+espelho.position.y = -1.5;
+espelho.rotateX( - Math.PI / 2 );
+cena.add( espelho );
+
 
 function animate() {
   root.rotation.y += 0.002;
@@ -62,4 +93,6 @@ window.addEventListener( 'resize', () => {
   camera.aspect = window.innerWidth / window.innerHeight;
   camera.updateProjectionMatrix();
   renderer.setSize( window.innerWidth, window.innerHeight );
+
+  espelho.getRenderTarget().setSize( window.innerWidth, window.innerHeight );
 });
